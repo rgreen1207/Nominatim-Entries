26d33afeccb89d9e546a9aacceea324ec2b10530
Ryan Green <ryan@rgreen.dev>
2024-11-01 14:53:02 -0700

    Create new_entries.py

---
+++ new_entries.py
@@ -0,0 +1,87 @@
+import time
+import osmium.osm
+
+from uuid import uuid4
+from datetime import datetime, timezone
+
+from database.db_models import AddressesDB
+from api.osm_imports.models.new_entry_models import NewNominatimEntry
+from api.address_lookups.helpers.normalization import AddressNormalization
+
+
+class NewEntryXMLActions:
+
+    @classmethod
+    async def create_new_entry(cls, data, filename=None):
+        filename = f'{uuid4()}.osm.xml' if filename == None else filename
+        with osmium.SimpleWriter(filename) as writer:
+            entry_id = round(time.time() * 1000)
+            if isinstance(data, list):
+                for entry in data:
+                    newEntry = await cls.populate_entry(entry, entry_id)
+                    entry_id+=1
+                    writer.add_node(newEntry)
+                    writer.add_node(cls.create_place_entry(newEntry, entry_id))
+                    entry_id+=1
+            else:
+                newEntry = await cls.populate_entry(data, entry_id)
+                writer.add_node(newEntry)
+                writer.add_node(cls.create_place_entry(newEntry, entry_id))        
+        return {
+            "status": 200,
+            "filename": filename
+        }
+
+
+    @classmethod
+    async def populate_entry(cls, data, entry_id):
+        if type(data) == AddressesDB:
+            data = AddressNormalization.parsed_street_to_model(data, AddressNormalization.parse_address(data.full_searchable))
+        else:
+            data = AddressNormalization.parsed_street_to_model(data, AddressNormalization.parse_address(data.create_full_str()))
+        newEntry = osmium.osm.mutable.Node(
+            timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            id = entry_id,
+            user = "osm_imports",
+            version = 1,
+            tags = cls.set_tags(data),
+            location = cls.set_location(data),
+            visible = True
+        )
+        return newEntry
+
+
+    @staticmethod
+    def set_tags(data):
+        tagList = []
+        if type(data) == NewNominatimEntry:
+            for key, value in data.__dict__.items():
+                if key in ['street', 'housenumber', 'unit', 'city', 'state', 'postcode', 'country']:
+                    tagList.append(osmium.osm.Tag(f"addr:{key}", str(value)))
+        elif type(data) == AddressesDB:
+            data.country = "US" if data.country == None else data.country
+            data.state = AddressNormalization.CODE_TO_STATE[data.state]
+            for key, value in data.__dict__.items():
+                if key in key in ['housenumber', 'street', 'city', 'state', 'country']:
+                    tagList.append(osmium.osm.Tag(f"addr:{key}", str(value)))
+                elif key == 'zip_code':
+                    tagList.append(osmium.osm.Tag(f"addr:postcode", str(value)))
+        return tagList
+    
+
+    @staticmethod
+    def set_location(data):
+        datakeys = data.__dict__.keys()
+        if 'lat' in datakeys and 'lon' in datakeys:
+            return (float(data.lon), float(data.lat))
+        if data.latitude and data.longitude:
+            return (float(data.longitude), float(data.latitude))
+        return (0, 0)
+    
+    
+    def create_place_entry(entry, entry_id):
+        entry.id = entry_id
+        for i in entry.tags:
+            if 'addr:street' in i: 
+                entry.tags[entry.tags.index(i)] = osmium.osm.Tag("addr:place", i[1])
+        return entry
